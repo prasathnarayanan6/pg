9bda401ac86b6f322d3eeb7988a421ea089b8e8a
prasathnarayanan6 <76641242+prasathnarayanan6@users.noreply.github.com>
2022-11-20 11:39:07 +0530

    Add files via upload

---
+++ pg.php
@@ -0,0 +1,334 @@
+<!DOCTYPE html>
+<html lang="en">
+   <head>
+      <meta charset="UTF-8">
+      <meta http-equiv="X-UA-Compatible" content="IE=edge">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <title>Product</title>
+      <link rel="icon" href="./images/xlogoo.png" type="images/xlogoo.png">
+      <link rel="stylesheet" href="css/style.css">
+      <link href="//netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.css" rel="stylesheet">
+      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css" integrity="sha512-MV7K8+y+gLIBoVD59lQIYicR65iaqukzvf/nwasF0nqhPay5w/9lJmVM2hMDcnK1OnMGCdVK+iQrJ7lzPJQd1w==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+
+      <!-- <link href="https://www.dafontfree.net/embed/Z290aGFtLWxpZ2h0JmRhdGEvNDYvZy82NDAzMC9Hb3RoYW0tTGlnaHQub3Rm" rel="stylesheet" type="text/css"/> -->
+      <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+      <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
+      <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
+
+      
+   </head>
+   <body>
+      <!-- <div id="loader"></div> -->
+      <!-----navbar-------->
+      <nav class="navbar navbar-expand-lg navbar-light ">
+         <div class="container-fluid">
+            <a class="navbar-brand" href="#"><img src="https://www.xyma.in/fimages/logo.png" width="40%"></a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+               <ul class="navbar-nav me-auto ms-auto mb-2 mb-lg-0">
+                  <li class="nav-item ms-auto">
+                     <a class="nav-link" aria-current="page" href="#"><b>Home</b></a>
+                  </li>
+                  <li class="nav-item ms-auto">
+                     <a class="nav-link" href="#"><b>Products</b></a>
+                  </li>
+                  <li class="nav-item ms-auto">
+                     <a class="nav-link" href="#">
+                     <b>Careers</b>
+                     </a>
+                  <li class="nav-item ms-auto">
+                     <a class="nav-link" href="#"><b>Spotlights</b></a>
+                  </li>
+                  <li class="nav-item ms-auto">
+                     <a class="nav-link" href="#"><b>About Us</b></a>
+                  </li>
+                  <li class="nav-item ms-auto">
+                     <a class="nav-link" href="#"><b>Contact Us</b></a>
+                  </li>
+               </ul>
+               <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+                  <li class="nav-item ms-auto">
+                     <a class="nav-link" href="#">
+                        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css" integrity="sha512-MV7K8+y+gLIBoVD59lQIYicR65iaqukzvf/nwasF0nqhPay5w/9lJmVM2hMDcnK1OnMGCdVK+iQrJ7lzPJQd1w==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+                        <i class="fa-solid fa-rocket"></i> Demo</i>
+                     </a>
+                  </li>
+               </ul>
+            </div>
+         </div>
+      </nav>
+      <br><br>
+      <!---navbar----->
+      <div class="container-fluid">
+      <div class="container mt-5">
+         <div class="row " >
+            <div class="col-md-9" style="background-color: white; border-radius:8px;">
+               <h1 class="fnt mt-2" id="effect"></h1>
+               <br><br>
+               <h6 class="fnt" id="">| Robust | Multi-Point sensing | Multi-Parameter sensing | Small footprint | Wide Temperature Range </h6>
+            </div>
+            <br><br>
+            <!-- <div class="col-md-6 col-sm-6">
+               <img class="img-fluid"  src="https://xyma.in/newwebsite/aimages/products/ztar.svg" style="border-radius:20px;">
+               </div> -->
+         </div>
+         <br><br><br><br>
+         <div class="row" style="">
+            <div class="col-md-12 mt-3">
+               <center>
+                  <h3 class="gradient-underline fnt">Products</h3>
+                  <br>
+               </center>
+            </div>
+         </div>
+         <div class="row" style=" border-radius: 5px; box-shadow: rgba(0, 0, 0, 0.1) 0px 0px 5px 0px, rgba(0, 0, 0, 0.1) 0px 0px 1px 0px;">
+            <div class="col-md-12 mt-3">
+               <div class="row">
+                  <div class="col-md-6" style="">
+                     <img src="./images/utmaps__1_-removebg-preview.png" class="img-fluid" style=""></img>
+                  </div>
+                  <div class="col-md-6 mt-5" style="">
+                     <center>
+                        <h3 class="fnt">
+                        μTMapS
+                        <h3>
+                     </center>
+                     <br>
+                     <center>
+                        <h5 class="fnt">
+                        Multi Point Temperature Mapping Sensor
+                        <h5>
+                     </center>
+                     <br>
+                     <center>
+                        <h6 class="fnt">They are robust high temperature sensors that are capable of multi-point measurement using waveguide sensors. The temperature sensor is based on XYMA’s patented ultrasonic waveguide technology.</h6>
+                     </center>
+                  </div>
+               </div>
+               <div class="row mt-6">
+                  <div class="col-md-8 mt-5" >
+                     <center>
+                        <h3 class="fnt">
+                        PoRTS
+                        <h3>
+                     </center>
+                     <br>
+                     <center>
+                        <h5 class="fnt">
+                        Portable Rheology and Temperature Sensor
+                        <h5>
+                     </center>
+                     <br>
+                     <center>
+                        <h6 class="fnt">PoRTS is a our multi-parameter sensor which measures the Viscosity, Temperature and Level of a fluid using a single ultrasonic waveguide with higher reliability and precision.</h6>
+                     </center>
+                  </div>
+                  <div class="col-md-4" style="">
+                     <img src="./images/ports.svg" class="img-fluid"></img>
+                  </div>
+               </div>
+               <div class="row">
+                  <div class="col-md-6" style="">
+                     <img src="./images/ztar-removebg-preview.png" class="img-fluid" style=""></img>
+                  </div>
+                  <div class="col-md-6 mt-5" style="">
+                     <center>
+                        <h3 class="fnt">
+                        μTMapS
+                        <h3>
+                     </center>
+                     <br>
+                     <center>
+                        <h5 class="fnt">
+                        Multi Point Temperature Mapping Sensor
+                        <h5>
+                     </center>
+                     <br>
+                     <center>
+                        <h6 class="fnt">They are robust high temperature sensors that are capable of multi-point measurement using waveguide sensors. The temperature sensor is based on XYMA’s patented ultrasonic waveguide technology.</h6>
+                     </center>
+                  </div>
+               </div>
+               <div class="row mt-6">
+                  <div class="col-md-6 mt-5" >
+                     <center>
+                        <h3 class="fnt">
+                        I-PAMS
+                        <h3>
+                     </center>
+                     <br>
+                     <center>
+                        <h5 class="fnt">
+                        IoT for Plant Asset Monitoring System
+                        <h5>
+                     </center>
+                     <br>
+                     <center>
+                        <h6 class="fnt">Industrial-IoT based asset monitoring system providing real time data visualization and detailed analysis.</h6>
+                     </center>
+                  </div>
+                  <div class="col-md-6" style="">
+                     <img src="./images/ipams.jpg" class="img-fluid"></img>
+                  </div>
+               </div>
+               <br>
+            </div>
+            <br><br>
+         </div>
+         <br><br>
+         <div class="row">
+              <div class="col-md-12">
+                  <center><h3 class="fnt gradient-underline" id="eft"></h3></center>
+              </div>
+         </div>
+         <div class="row">
+              <div class="col-md-6 inline-button">
+                 <div id="outer">
+                      <div class="inner"><button type="submit" class="msgBtn btn btn-warning btn-lg fnt mt-1" style="border-radius: 25px;" >Get In Touch</button></div>
+                </div>
+              </div>
+              <div class="col-md-6 inline-button">
+                 <div id="outer">
+                      <div class="inner"><button type="submit" class="msgBtn btn btn-warning btn-lg fnt mt-1" style="border-radius: 25px;" >Download Brouchure</button></div>
+                </div>
+              </div>
+              <br><br><br>
+        </div>
+      <div class="cursor"></div>
+      <div class="cursor2"></div>
+      <!-----nav bar over------>
+      <!----fooooterr-------------------->
+      <footer class="footer">
+  	 <div class="container">
+  	 	<div class="row">
+  	 		<div class="footer-col">
+  	 			<h4>company</h4>
+  	 			<ul>
+  	 				<li><a href="#">about us</a></li>
+  	 				<li><a href="#">our services</a></li>
+  	 				<li><a href="#">privacy policy</a></li>
+  	 				<li><a href="#">affiliate program</a></li>
+  	 			</ul>
+  	 		</div>
+  	 		<div class="footer-col">
+  	 			<h4>get help</h4>
+  	 			<ul>
+  	 				<li><a href="#">FAQ</a></li>
+  	 				<li><a href="#">shipping</a></li>
+  	 				<li><a href="#">returns</a></li>
+  	 				<li><a href="#">order status</a></li>
+  	 				<li><a href="#">payment options</a></li>
+  	 			</ul>
+  	 		</div>
+  	 		<div class="footer-col">
+  	 			<h4>online shop</h4>
+  	 			<ul>
+  	 				<li><a href="#">watch</a></li>
+  	 				<li><a href="#">bag</a></li>
+  	 				<li><a href="#">shoes</a></li>
+  	 				<li><a href="#">dress</a></li>
+  	 			</ul>
+  	 		</div>
+  	 		<div class="footer-col">
+  	 			<h4>follow us</h4>
+  	 			<div class="social-links">
+  	 				<a href="#"><i class="fab fa-facebook-f"></i></a>
+  	 				<a href="#"><i class="fab fa-twitter"></i></a>
+  	 				<a href="#"><i class="fab fa-instagram"></i></a>
+  	 				<a href="#"><i class="fab fa-linkedin-in"></i></a>
+  	 			</div>
+  	 		</div>
+  	 	</div>
+  	 </div>
+  </footer>
+  
+
+
+
+   </body>
+   <script>
+      var cursor = document.querySelector('.cursor');
+      var cursorinner = document.querySelector('.cursor2');
+      var a = document.querySelectorAll('a');
+      
+      document.addEventListener('mousemove', function(e){
+        var x = e.clientX;
+        var y = e.clientY;
+        cursor.style.transform = `translate3d(calc(${e.clientX}px - 50%), calc(${e.clientY}px - 50%), 0)`
+      });
+      
+      document.addEventListener('mousemove', function(e){
+        var x = e.clientX;
+        var y = e.clientY;
+        cursorinner.style.left = x + 'px';
+        cursorinner.style.top = y + 'px';
+      });
+      
+      document.addEventListener('mousedown', function(){
+        cursor.classList.add('click');
+        cursorinner.classList.add('cursorinnerhover')
+      });
+      
+      document.addEventListener('mouseup', function(){
+        cursor.classList.remove('click')
+        cursorinner.classList.remove('cursorinnerhover')
+      });
+      
+      a.forEach(item => {
+        item.addEventListener('mouseover', () => {
+          cursor.classList.add('hover');
+        });
+        item.addEventListener('mouseleave', () => {
+          cursor.classList.remove('hover');
+        });
+      })
+      // var loader;
+      // function loadNow(opacity){
+      //   if(opacity <= 0){
+      //     displayContent();
+      //   }
+      //   else{
+      //     loader.style.opacity = opacity;
+      //     window.setTimeout(function(){
+      //       loadNow(opacity - 0.01)
+      //     }, 100);
+      //   }
+      // }
+      // function displayContent(){
+      //   loader.style.display = 'none';
+      //   document.getelementById('content').style.display = 'block';
+      // }
+      // document.addEventListener("DOMContentLoaded", function(){
+      //   loader = document.getElementById('loader');
+      //   loader(1);
+      // });
+      let x = 0;
+      let textEffect = "Our SENSOR Solutions Suit all of \n your needs.";
+      let container = document.getElementById("effect");
+      
+      function animate() {
+          if(x < textEffect.length){
+              container.innerHTML += textEffect.charAt(x);
+              x++;
+              setTimeout(animate, 35);
+          }
+      }
+      animate();
+
+      let y = 0;
+      let te = "Let's Protect your Process while Increasing your Productivity";
+      let cont = document.getElementById("eft");
+      
+      function ani() {
+          if(y < te.length){
+              cont.innerHTML += te.charAt(y);
+              y++;
+              setTimeout(ani, 70);
+          }
+      }
+      ani();
+      
+   </script>
+</html>
